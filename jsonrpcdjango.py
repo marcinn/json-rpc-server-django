89e8c412825901b7b4f35086661082621949bf82
Marcin Nowak <marcin.j.nowak@gmail.com>
2015-03-23 02:19:11 +0100

    first commit

---
+++ jsonrpcdjango.py
@@ -0,0 +1,31 @@
+from django.http import HttpResponse
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+from django.views.generic import View
+
+
+class RpcServerView(View):
+    server_instance = None
+
+    def __init__(self, *args, **kw):
+        super(RpcServerView, self).__init__(*args, **kw)
+        self._rpc_server = self.server_instance
+
+    def post(self, request):
+        result = self._rpc_server.handle_http_request(request)
+        return HttpResponse(result)
+
+    @method_decorator(csrf_exempt)
+    def dispatch(self, *args, **kwargs):
+        return super(RpcServerView, self).dispatch(*args, **kwargs)
+
+
+def csrf_serve(request, service):
+    result = service.handle_http_request(request)
+    return HttpResponse(result)
+
+
+@csrf_exempt
+def serve(request, service):
+    return csrf_serve(request, service)
+
---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(name='damn-simple-jsonrpc-server-django',
+      version='0.1',
+      description='Django adapter for damn simple JSON-RPC Server',
+      classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Web Environment",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      author='Marcin Nowak',
+      author_email='marcin.j.nowak@gmail.com',
+      url='',
+      keywords='web json rpc python server django',
+      py_modules=['jsonrpcdjango'],
+      include_package_data=True,
+      zip_safe=False,
+      )
+
